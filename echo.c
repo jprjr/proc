74b0fbc26f39c4a3f28596c8ececb16cd9131a0b
John Regan <john@jrjrtech.com>
2019-06-07 17:07:24 -0400

    make echo use binary mode, get same output on linux/windows

--- echo.c
+++ echo.c
@@ -1,8 +1,15 @@
 /* just echoes arguments */
 #include <stdio.h>
+#ifdef _WIN32
+#include <io.h>
+#include <fcntl.h>
+#endif
 
 int main(int argc, char *argv[]) {
     int i=1;
+#ifdef _WIN32
+    _setmode(fileno(stdout),O_BINARY);
+#endif
     while(i<argc) {
         printf("argv[%d]: %s\n",i,argv[i]);
         i++;
